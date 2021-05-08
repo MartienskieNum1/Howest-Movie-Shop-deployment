8ce8d7e15ea0a94ad1d3117d3ddb40fe35247173
Maarten De Meyere <maarten.de.meyere@student.howest.be>
2021-05-08 18:31:34 +0200

    create deployment script

--- project/deploy.py
+++ project/deploy.py
@@ -1,7 +1,18 @@
 import os
 
 commands = [
-    # your deploy commands
+'git clone git@git.ti.howest.be:TI/2020-2021/s4/dotnet-technology/cccp-ibc/project/students/maarten-de-meyere/app.git',
+'git clone git@git.ti.howest.be:TI/2020-2021/s4/dotnet-technology/cccp-ibc/project/students/maarten-de-meyere/lib.git',
+
+'dotnet new sln',
+'dotnet sln add app/app.csproj',
+'dotnet sln add lib/lib.csproj',
+'dotnet add "app/app.csproj" reference lib/lib.csproj',
+
+'dotnet clean',
+'dotnet build',
+
+'dotnet run --project app/app.csproj'
 ]
 
 os.system("rm -rf build")
